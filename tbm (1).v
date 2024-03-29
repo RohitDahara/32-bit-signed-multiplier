d1af75191946dfe7a722b8d0c70d92c93341eae7
Rohit <122045095+RohitDahara@users.noreply.github.com>
2024-03-29 20:37:25 +0530

    Add files via upload

---
+++ tbm (1).v
@@ -0,0 +1,36 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 08.03.2024 02:21:37
+// Design Name: 
+// Module Name: tbm
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module tbm();
+reg [31:0] A,B;
+wire [63:0] R;
+
+mul32bit mm1(.a(A), .b(B), .p(R));
+initial
+begin
+A = 8 ; B = 125;
+#100;
+
+A = 225; B = 30;  
+end
+
+endmodule
